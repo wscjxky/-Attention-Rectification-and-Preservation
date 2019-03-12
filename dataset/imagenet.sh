0046232e07f7c7daa7d2d4ed3e257de4681d8939
619677632@qq.com <619677632@qq.com>
2019-03-12 11:46:11 +0800

    init

---
+++ dataset/imagenet.sh
@@ -0,0 +1,1 @@
+wget -c (http://www.image-npet.org/challenges/LSVRC/2012/nnoupb/ILSVRC2012_img_val.tar)
---
+++ model/inceptionv3.sh
@@ -0,0 +1,2 @@
+curl -O http://download.tensorflow.org/models/image/imagenet/inception-v3-2016-03-01.tar.gz
+tar xzf inception-v3-2016-03-01.tar.gz
